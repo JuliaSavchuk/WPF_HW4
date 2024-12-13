eb2d1f361ac99324a596531adec09009d421a73f
MSI <MSI@DESKTOP-Q06R7EA>
2024-12-14 00:22:10 +0200

    Добавьте файлы проекта.

---
+++ WPF_HW4/Game.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPF_HW4
+{
+    public static class Game
+    {
+
+        private static char[] field = new char[] { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', };
+
+        public static char CurrentPlayer { get; private set; } = 'X';
+
+
+        public static bool IsTaken(int cellIndex)
+        {
+            return field[cellIndex] != ' ';
+        }
+
+        public static void Turn(int cellIndex)
+        {
+            field[cellIndex] = CurrentPlayer;
+        }
+
+        public static void ChangePlayer()
+        {
+            CurrentPlayer = CurrentPlayer == 'X' ? 'O' : 'X';
+        }
+
+        public static char? GetWinner()
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                if (field[i * 3] != ' ' && field[i * 3] == field[i * 3 + 1] && field[i * 3] == field[i * 3 + 2])
+                {
+                    return CurrentPlayer;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (field[i] != ' ' && field[i] == field[i + 3] && field[i] == field[i + 6])
+                {
+                    return CurrentPlayer;
+                }
+            }
+
+            if (field[0] != ' ' && field[0] == field[4] && field[0] == field[8])
+            {
+                return CurrentPlayer;
+            }
+            if (field[2] != ' ' && field[2] == field[4] && field[2] == field[6])
+            {
+                return CurrentPlayer; 
+            }
+
+            return null;
+        }
+
+        public static bool IsTie()
+        {
+            return GetWinner() == null && !field.Contains(' ');
+        }
+
+        public static void ResetField()
+        {
+            for (int i = 0; i < field.Length; i++)
+            {
+                field[i] = ' ';
+            }
+            CurrentPlayer = 'X';
+        }
+
+    }
+}
---
+++ WPF_HW4/MainWindow.xaml.cs
@@ -0,0 +1,85 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WPF_HW4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private int playerXScore = 0;
+        private int playerOScore = 0;
+        private int isTie = 0;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        public void OnButtonClick(object sender, RoutedEventArgs e)
+        {
+            Button pressedButton = (Button)sender;
+
+            int cellIndex = Convert.ToInt32(pressedButton.Tag);
+
+            if (Game.IsTaken(cellIndex))
+                return;
+
+            Game.Turn(cellIndex);
+
+            pressedButton.Content = Game.CurrentPlayer;
+
+            char? winner = Game.GetWinner();
+
+            if ((winner) != null)
+            {
+                MessageBox.Show($"Player '{winner}' winner!");
+                if (winner == 'X')
+                {
+                    playerXScore++;
+                    PlayerXScore.Content = $"Player X: {playerXScore}";
+                }
+                else if (winner == 'O')
+                {
+                    playerOScore++;
+                    PlayerOScore.Content = $"Player O: {playerOScore}";
+                }
+
+                ResetGame();
+            }
+            else if (Game.IsTie())
+            {
+                MessageBox.Show("It's a tie!");
+
+                isTie++;
+                DrawGame.Content = $"Draw: {isTie}";
+                ResetGame();
+            }
+            else
+            {
+                Game.ChangePlayer();
+            }
+        }
+
+        private void ResetGame()
+        {
+            Game.ResetField();
+
+            foreach (var child in MainGrid.Children)
+            {
+                if (child is Button button && button.Tag != null)
+                {
+                    button.Content = string.Empty;
+                }
+            }
+        }
+    }
+}
